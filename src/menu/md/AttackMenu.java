8c7ff91d2d835969e40982ded661bc92cffb41a2
jorgejimene <113776889+jorgejimene@users.noreply.github.com>
2024-11-28 21:30:57 +0100

    Ataque distinguido

--- src/menu/md/AttackMenu.java
+++ src/menu/md/AttackMenu.java
@@ -1,29 +1,52 @@
 package menu.md;
 
-import menu.md.enums.AttackMenuOptions;
+import entes.md.*;
+import menu.md.enums.AttackMenuOptionsMonstruo;
+import menu.md.enums.AttackMenuOptionsPersona;
 
 public class AttackMenu extends Menu {
+	private Ente e;
+	public AttackMenu(Ente e) {
+		this.e=e;
+	}
 
     @Override
     public String toString() {
     	// TODO Auto-generated method stub
     	String exit = "";
-    	for (AttackMenuOptions option : AttackMenuOptions.values()) {
-    		exit += option + "\n";
-        }
-    	return exit;
+		if(this.e instanceof Monstruo) {
+			for (AttackMenuOptionsMonstruo option : AttackMenuOptionsMonstruo.values()) {
+				exit += option + "\n";
+			}
+		}
+		else if(this.e instanceof Persona) {
+			for (AttackMenuOptionsPersona option : AttackMenuOptionsPersona.values()) {
+				exit += option + "\n";
+			}
     }
+		return exit;
+}
 
 
 	@Override
 	public Integer[] getOptions() {
+		Integer[] options = null;
 		// TODO Auto-generated method stub
-		AttackMenuOptions[] optionsAM = AttackMenuOptions.values();
-		Integer[] options = new Integer[optionsAM.length];
-		for(int i=0; i<optionsAM.length;i++) {
-			options[i] = optionsAM[i].getOption();
+		if(this.e instanceof Monstruo) {
+			AttackMenuOptionsMonstruo[] optionsAM = AttackMenuOptionsMonstruo.values();
+			options = new Integer[optionsAM.length];
+			for(int i=0; i<optionsAM.length;i++) {
+				options[i] = optionsAM[i].getOption();
+			}
+		} else if(this.e instanceof Persona) {
+			AttackMenuOptionsPersona[] optionsAP = AttackMenuOptionsPersona.values();
+			options = new Integer[optionsAP.length];
+			for(int i=0; i<optionsAP.length;i++) {
+				options[i] = optionsAP[i].getOption();
+			}
 		}
  		return options;
+ 		
 	}
 
 	@Override
