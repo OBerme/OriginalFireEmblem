e9d8f1e265d5577e6a05cb8d3ee2f81e36b8c0d1
OBerme <oscarestudios2001@gmail.com>
2025-03-21 11:04:22 +0100

    Solved problem distance and animation

--- src/presentation/map/jbutton/PGraphicPositionIntegerAtack.java
+++ src/presentation/map/jbutton/PGraphicPositionIntegerAtack.java
@@ -1,6 +1,10 @@
 package presentation.map.jbutton;
 
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+import presentation.MouseHoverObserver.IMouseHoverSubject;
 import presentation.main.PDefaultValues;
 import presentation.map.IPPPositionSubjectData;
 import presentation.map.position.IGraphicPosition;
@@ -13,17 +17,29 @@ public class PGraphicPositionIntegerAtack
 	extends PGraphicPositionInteger implements IPGraphicPositionIntegerAtack{
 	
 	protected boolean actiAtack; //Shows if the cell is activate for an atack
-	
-	
+
+	protected IMouseHoverSubject subjectMouse; 
 	
 	public PGraphicPositionIntegerAtack(IGraphicPosition<Integer, Integer> gPosition,
 			IPPPositionSubjectData pSubject,
-			IJButtonAction action) {
+			IJButtonAction action,IMouseHoverSubject subjectMouse) {
 		super(gPosition, pSubject, action);
 		this.actiAtack = false;
+		this.subjectMouse = subjectMouse;
 		
-		
-
+		addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+             	onMouseMotion(e);
+            }
+        });
+	}
+	
+	private void onMouseMotion(MouseEvent e) {
+		if(PDefaultValues.DEBUG_MODE_PGPIAD) System.out.println("Positions reached atack" + gPosition);
+    	if(actiAtack) {
+    		subjectMouse.setPosition(gPosition);
+    	}
 	}
 
 	@Override
